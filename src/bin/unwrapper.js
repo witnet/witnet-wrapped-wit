1a557c3033f0fb8e5da5339f2b0c1b5172c28e4f
Guillermo Díaz <guillermo@witnet.io>
2025-07-31 09:02:42 +0200

    fix(unwrapper): split utxos upon start, if required

--- src/bin/unwrapper.js
+++ src/bin/unwrapper.js
@@ -50,7 +50,7 @@ async function main() {
 
     const minBalance = Witnet.Coins.fromWits(WIT_MIN_BALANCE)
 
-    let balance = 0n
+    let balance = Witnet.Coins.fromPedros(0n)
     balance = await checkWitnetBalance()
     console.info(`Initial balance:   ${balance.toString(2)} (${signer.cacheInfo.size} UTXOs)`)
     if (balance.pedros < minBalance.pedros) {
@@ -281,7 +281,7 @@ async function main() {
     async function checkWitnetBalance() {
         let newBalance = Witnet.Coins.fromPedros((await signer.getBalance()).unlocked)
         let now = Math.floor(Date.now() / 1000)
-        let increased = newBalance.nanowits > balance.nanowits
+        let increased = newBalance.nanowits > balance?.nanowtis || 0n
         let utxos = (await signer.getUtxos(increased)).filter(utxo => utxo.timelock <= now)
         if (increased && utxos.length < WIT_MIN_UTXOS) {
             const splits = Math.min(WIT_MIN_UTXOS * 2, 50)
